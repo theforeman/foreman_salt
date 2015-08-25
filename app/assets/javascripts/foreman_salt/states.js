196be10365fa516e7591343dbfde5c9a27434603
Stephen Benjamin <stbenjam@redhat.com>
2015-08-24 21:00:54 -0400

    Address javascript issues

--- app/assets/javascripts/foreman_salt/states.js
+++ app/assets/javascripts/foreman_salt/states.js
@@ -3,18 +3,20 @@ $(document).on( "ContentLoad", function() {
 });
 
 function update_salt_states(element) {
-  var host_id = $("form").data('id')
+  var host_id = $("form").data('id');
   var env_id = $('select[name*=salt_environment_id]').val();
   var url = $(element).attr('data-url');
   var data = $("form").serialize().replace('method=put', 'method=post');
 
   if (url.match('hostgroups')) {
-    data = data + '&hostgroup_id=' + host_id
+    data = data + '&hostgroup_id=' + host_id;
   } else {
-    data = data + '&host_id=' + host_id
+    data = data + '&host_id=' + host_id;
   }
 
-  if (env_id == "") return;
+  if (!env_id) {
+    return;
+  }
 
   $(element).indicator_show();
   $.ajax({
