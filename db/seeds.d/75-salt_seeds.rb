45bfecba60157a3b11121db2d86a348bc8a84753
James Shewey <jdshewey@gmail.com>
2017-11-22 04:46:02 -0700

    Refs #20055 - implement workaround for role seeding

--- db/seeds.d/75-salt_seeds.rb
+++ db/seeds.d/75-salt_seeds.rb
@@ -6,6 +6,8 @@ viewer = Role.find_by_name('Viewer')
 
 if viewer
   Permission.where(:name => [:view_smart_proxies_salt_keys, :view_smart_proxies_salt_autosign, :view_salt_modules]).each do |permission|
-    viewer.add_permissions!([permission.name]) unless viewer.permissions.include? permission
+    viewer.ignore_locking do
+      viewer.add_permissions!([permission.name]) unless viewer.permissions.include? permission
+    end
   end
 end
