e729f9c1034bc271114db642e7ae716cdc8c0b29
Michael Moll <kvedulv@kvedulv.de>
2018-01-05 06:10:39 +0100

    Reorder AR associations in order of use

--- app/models/foreman_salt/concerns/host_managed_extensions.rb
+++ app/models/foreman_salt/concerns/host_managed_extensions.rb
@@ -4,8 +4,8 @@ module ForemanSalt
       extend ActiveSupport::Concern
 
       included do
-        has_many :salt_modules, :through => :host_salt_modules, :class_name => '::ForemanSalt::SaltModule'
         has_many :host_salt_modules, :foreign_key => :host_id, :class_name => '::ForemanSalt::HostSaltModule'
+        has_many :salt_modules, :through => :host_salt_modules, :class_name => '::ForemanSalt::SaltModule'
 
         belongs_to :salt_proxy, :class_name => 'SmartProxy'
         belongs_to :salt_environment, :class_name => 'ForemanSalt::SaltEnvironment'
