a150876fddf6eba2ffa9648b9569e0e86a728504
Bernhard Suttner <sbernhard@users.noreply.github.com>
2019-07-18 12:03:50 +0200

    More rubocop fixes (#107)
    Thanks @rahulbajaj0509

--- db/migrate/20150220122707_fix_incorrect_report_metrics.rb
+++ db/migrate/20150220122707_fix_incorrect_report_metrics.rb
@@ -2,6 +2,7 @@ class FixIncorrectReportMetrics < ActiveRecord::Migration[4.2]
   def up
     Report.all.each do |report|
       next unless report.metrics && report.metrics['time']
+
       metrics = report.metrics.dup
 
       report.metrics['time'].each do |resource, time|
