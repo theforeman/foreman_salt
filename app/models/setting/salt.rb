1966b12bd66a6180cc44f210bed2b334ab7e6d7f
Ondřej Ezr <oezr@redhat.com>
2019-09-10 18:47:16 +0200

    Fixes #27821 - setting initialization improvement

--- app/models/setting/salt.rb
+++ app/models/setting/salt.rb
@@ -1,14 +1,13 @@
 class Setting::Salt < Setting
-  def self.load_defaults
-    return unless super
+  def self.default_settings
+    [
+      set('salt_namespace_pillars', N_("Namespace Foreman pillars under 'foreman' key"), false),
+      set('salt_hide_run_salt_button', N_("Hide the Run Salt state.highstate button on the host details page"), false)
+    ]
+  end
 
-    transaction do
-      [
-        set('salt_namespace_pillars', N_("Namespace Foreman pillars under 'foreman' key"), false),
-        set('salt_hide_run_salt_button', N_("Hide the Run Salt state.highstate button on the host details page"), false)
-      ].each { |s| self.create! s.update(:category => 'Setting::Salt') }
-      Setting['salt_hide_run_salt_button'] = true if ForemanSalt.with_remote_execution?
-    end
-    true
+  def self.load_defaults
+    super
+    Setting['salt_hide_run_salt_button'] = true if ForemanSalt.with_remote_execution?
   end
 end
