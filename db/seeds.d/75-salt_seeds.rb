59786faf1d485782c20584c9b79097b482192ab7
Bernhard Suttner <sbernhard@users.noreply.github.com>
2019-11-18 15:49:44 +0100

    Fixes #20055 - add salt viewer role (#121)

--- db/seeds.d/75-salt_seeds.rb
+++ db/seeds.d/75-salt_seeds.rb
@@ -1,13 +1,2 @@
 # Create feature for Smart Proxy
 Feature.where(:name => 'Salt').first_or_create
-
-# Add new viewing permissions to Viewer role
-viewer = Role.find_by_name('Viewer')
-
-if viewer
-  Permission.where(:name => [:view_smart_proxies_salt_keys, :view_smart_proxies_salt_autosign, :view_salt_modules]).each do |permission|
-    viewer.ignore_locking do
-      viewer.add_permissions!([permission.name]) unless viewer.permissions.include? permission
-    end
-  end
-end
