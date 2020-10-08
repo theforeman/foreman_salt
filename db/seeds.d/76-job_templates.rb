78e78ea981bc173a7280ada6db477e0ad79ce108
Quirin Pamp <quirinp@protonmail.com>
2020-10-08 15:23:30 +0200

    Ensure the default job templates are locked (#137)

--- db/seeds.d/76-job_templates.rb
+++ db/seeds.d/76-job_templates.rb
@@ -20,7 +20,7 @@ if ForemanSalt.with_remote_execution?
           sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
           template = JobTemplate.import_raw!(File.read(template),
                                              :default => true,
-                                             :locked => true,
+                                             :lock => true,
                                              :update => sync)
           template.organizations = organizations if template.present?
           template.locations = locations if template.present?
