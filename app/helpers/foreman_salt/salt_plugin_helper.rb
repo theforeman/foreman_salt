bf365c9e6f67fefab2be08417e5064baca19008f
Maximilian <kolb@atix.de>
2022-04-20 20:26:01 +0200

    Use Foreman documentation instead of Manual (#168)
    The official documentation for foreman_salt is now in the Foreman
    documentation at docs.theforeman.org.

--- app/helpers/foreman_salt/salt_plugin_helper.rb
+++ app/helpers/foreman_salt/salt_plugin_helper.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-require "#{ForemanSalt::Engine.root}/lib/foreman_salt/version"
-
 module ForemanSalt
   # General helper functions for foreman_salt
   module SaltPluginHelper
     def salt_doc_url
-      major_version = ::ForemanSalt::VERSION.split('.')[0]
-      "https://theforeman.org/plugins/foreman_salt/#{major_version}.x/index.html"
+      'https://docs.theforeman.org/nightly/Managing_Hosts/index-foreman-el.html#Using_Salt_for_Configuration_Management_managing-hosts'
     end
   end
 end
