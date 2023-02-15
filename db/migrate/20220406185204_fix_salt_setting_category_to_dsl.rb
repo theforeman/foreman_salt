6eba7c6f62cd5a4c6856c42db0c140ef006937f0
Leos Stejskal <github@stejskalleos.cz>
2023-02-15 18:38:02 +0100

    Deal with the settings table not having a category column

--- db/migrate/20220406185204_fix_salt_setting_category_to_dsl.rb
+++ db/migrate/20220406185204_fix_salt_setting_category_to_dsl.rb
@@ -1,5 +1,5 @@
 class FixSaltSettingCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
-    Setting.where(category: 'Setting::Salt').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::Salt').update_all(category: 'Setting') if column_exists?(:settings, :category)
   end
 end
