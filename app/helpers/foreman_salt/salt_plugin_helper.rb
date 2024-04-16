f372f48e03638a6e4f668aff034e4a7583382ea9
Nadja Heitmann <nadjah@atix.de>
2024-04-16 11:41:10 +0200

    Update documentation link (#195)

--- app/helpers/foreman_salt/salt_plugin_helper.rb
+++ app/helpers/foreman_salt/salt_plugin_helper.rb
@@ -4,7 +4,7 @@ module ForemanSalt
   # General helper functions for foreman_salt
   module SaltPluginHelper
     def salt_doc_url
-      'https://docs.theforeman.org/nightly/Managing_Hosts/index-foreman-el.html#Using_Salt_for_Configuration_Management_managing-hosts'
+      'https://docs.theforeman.org/nightly/Managing_Configurations_Salt/index-foreman-el.html'
     end
   end
 end
