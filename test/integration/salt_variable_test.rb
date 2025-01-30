54d0264feef4a6e897359ccd2c670a4a02ad9e72
Evgeni Golov <evgeni@golov.de>
2025-01-30 08:46:06 +0100

    use select instead of select2 in tests

--- test/integration/salt_variable_test.rb
+++ test/integration/salt_variable_test.rb
@@ -18,7 +18,7 @@ module ForemanSalt
 
       assert_new_button(salt_variables_path, 'New Salt Variable', new_salt_variable_path)
       fill_in 'foreman_salt_salt_variable_key', with: 'mykey'
-      select2(state.name, from: 'foreman_salt_salt_variable_salt_module_id')
+      select(state.name, from: 'foreman_salt_salt_variable_salt_module_id')
 
       assert_submit_button(salt_variables_path)
       assert page.has_link? 'mykey'
